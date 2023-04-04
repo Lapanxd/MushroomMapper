93bbe65864b8960c6d118296eaf31450850ff748
Lapanxd <morgan.dumas@outlook.fr>
2023-04-04 11:33:04 +0200

    ajout de la library leaflet et création du component de la map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss']
+})
+export class MapComponent implements OnInit {
+
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ 46.227638, 2.213749 ],
+      zoom: 6
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+}
